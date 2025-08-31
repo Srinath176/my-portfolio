8a0b8134c353cc43beb5fb6632a47db39f9023b2
Srinath176 <srinathsri176@gmail.com>
2025-08-31 11:52:30 +0530

    optimize: performance optimization, lazy loading, preloads were added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import AnimatedBlobBackground from "./components/AnimatedBlobBg";
 import Header from "./components/Header";
 import Profile from "./components/Profile";
 import Skills from "./components/Skills";
@@ -6,12 +5,23 @@ import Projects from "./components/Projects";
 import GetInTouch from "./components/GetInTouch";
 import AnimatedSection from "./components/AnimatedSection";
 import Experience from "./components/Experience";
+import { lazy, Suspense } from "react";
+
+const AnimatedBlobBackground = lazy(() => import("./components/AnimatedBlobBg"));
 
 function App() {
   return (
     <div className="min-h-screen overflow-hidden">
-      <AnimatedBlobBackground />
-
+      <Suspense
+        fallback={
+          <div
+            aria-hidden
+            className="pointer-events-none h-40 md:h-56 lg:h-72"
+          />
+        }
+      >
+        <AnimatedBlobBackground />
+      </Suspense>
       {/* Treat Header as the Home section */}
       <div id="home">
         <Header />
